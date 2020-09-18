f3c7aec0805c7bfe0373fedd8a74b23dd7b426c2
hi-matbub <6matbub@gmail.com>
2020-09-17 20:47:00 -0700

    feat: mkdir

---
+++ src/index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const program = require('commander');
+const fs = require('fs-extra');
+const shell = require('shelljs');
+
+const engine = require('../package.json').engines.node;
+
+process.chdir(__dirname);
+
+program.version(engine);
+
+const CWD = process.cwd();
+console.log(CWD)
+
+const node = { 
+  index: `${CWD}/lib/node/index.js`
+}
+
+const runBuild = () => {
+  
+  shell.cd(CWD);
+  shell.mkdir('_server');
+
+  console.log(chalk.green('Initialized _server directory'));
+
+  shell.cd('_server');
+
+  console.log(
+    chalk.yellow(
+      'Building Express app in',
+      `${CWD}/_server`,
+    ),
+  );
+
+  fs.copyFileSync(node.index, './index.js', (err) => console.log(err));
+
+
+}
+
+runBuild()
+
+module.exports = { build: () => runBuild } 
